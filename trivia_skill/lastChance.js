04167b3e70ecf11167acac4a8753fe7735cc10d9
seedubgh1 <carltonsaddress@hotmail.com>
2017-06-02 10:16:39 -0700

    updated lastChance.js

--- trivia_skill/lastChance.js
+++ trivia_skill/lastChance.js
@@ -1,8 +1,34 @@
+// var request = require("request");
+// var quest = {};
+//
+// request("https://opentdb.com/api.php?amount=5", function(error, response, data) {
+//     body = data;
+//     quest = JSON.parse(body);
+// });
+// console.log(quest);
+// var request = require("request");
+// var body = {};
+//
+//
+// request("https://opentdb.com/api.php?amount=5", function(error, response, data) {
+//     body = data;
+//     console.log(body);
+// });
+//
+// console.log(body);
 var request = require("request");
-var quest = {};
+var EventEmitter = require("events").EventEmitter;
+var body = new EventEmitter();
+var bdy = {};
+
 
 request("https://opentdb.com/api.php?amount=5", function(error, response, data) {
-    body = data;
-    quest = JSON.parse(body);
+    body.data = data;
+    body.emit('update');
 });
-console.log(quest);
+
+body.on('update', function () {
+    console.log('on upd: ',body.data); // HOORAY! THIS WORKS!
+});
+bdy = body.on;
+console.log('bdy: ',bdy);
